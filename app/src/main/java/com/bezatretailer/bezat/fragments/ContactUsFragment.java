36aa25f6a77673813933a403763a719316b818c4
navjot rana <navjotrana28@gmail.com>
2019-12-02 01:25:13 +0530

    add back button in contact screen

--- app/src/main/java/com/bezatretailer/bezat/fragments/ContactUsFragment.java
+++ app/src/main/java/com/bezatretailer/bezat/fragments/ContactUsFragment.java
@@ -2,6 +2,7 @@ package com.bezatretailer.bezat.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.ImageView;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import com.bezatretailer.bezat.R;
 
 
 public class ContactUsFragment extends Fragment {
+    ImageView imgBack;
 
     public ContactUsFragment() {
         // Required empty public constructor
@@ -27,7 +29,14 @@ public class ContactUsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_us, container, false);
+        View view = inflater.inflate(R.layout.fragment_contact_us, container, false);
+        onClickBackButton(view);
+        return view;
+    }
+
+    private void onClickBackButton(View view) {
+        imgBack = view.findViewById(R.id.img_back);
+        imgBack.setOnClickListener(view1 -> getActivity().onBackPressed());
     }
 
     @Override
