905a895f7147add27e9eb3e4106cdf3b5e1f3a56
@NavjotRana <navjot@everwell.org>
2020-03-10 13:53:29 +0530

    prizes bug fix

--- app/src/main/java/com/bezatretailer/bezat/fragments/ContactUsFragment.java
+++ app/src/main/java/com/bezatretailer/bezat/fragments/ContactUsFragment.java
@@ -23,7 +23,7 @@ public class ContactUsFragment extends Fragment {
     ImageView imgBack;
     EditText name, email, comments;
     Button sendBtn;
-    private ImageView insta,youtube;
+    private ImageView insta,youtube,twitter,whatsapp,dialer;
 
     public ContactUsFragment() {
         // Required empty public constructor
@@ -79,6 +79,9 @@ public class ContactUsFragment extends Fragment {
         email = view.findViewById(R.id.contact_email_edit_text);
         insta = view.findViewById(R.id.contact_insta);
         youtube = view.findViewById(R.id.contact_browser);
+        twitter = view.findViewById(R.id.contact_twitter);
+        dialer = view.findViewById(R.id.contact_dialer);
+        whatsapp = view.findViewById(R.id.contact_whattsApp);
         comments = view.findViewById(R.id.contact_comments_edit_text);
         sendBtn = view.findViewById(R.id.contact_button);
         imgBack = view.findViewById(R.id.img_back);
@@ -94,6 +97,30 @@ public class ContactUsFragment extends Fragment {
             public void onClick(View view) {
                 startActivity( new Intent(Intent.ACTION_VIEW, Uri.parse
                         ("https://m.youtube.com/watch?v=PH8T9aZrRqg")));
+            }
+        });
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity( new Intent(Intent.ACTION_VIEW, Uri.parse
+                        ("https://twitter.com/bezatapp?s=08")));
+            }
+        });
+        dialer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:+97313399993"));
+                startActivity(intent);
+            }
+        });
+        whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("smsto:" + "+97313399993");
+                Intent i = new Intent(Intent.ACTION_SENDTO, uri);
+                i.setPackage("com.whatsapp");
+                startActivity(Intent.createChooser(i, "+97313399993"));
             }
         });
     }
