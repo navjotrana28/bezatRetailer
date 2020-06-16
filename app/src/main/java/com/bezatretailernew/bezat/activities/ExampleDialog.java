a54b64a803c3e6f8f1c8d5c2c2bd26a930e59eb9
@NavjotRana <navjot@everwell.org>
2020-06-16 21:57:51 +0530

    bug fixes

--- app/src/main/java/com/bezatretailernew/bezat/activities/ExampleDialog.java
+++ app/src/main/java/com/bezatretailernew/bezat/activities/ExampleDialog.java
@@ -7,13 +7,14 @@ import android.os.Bundle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+import com.bezatretailernew.bezat.R;
+
 public class ExampleDialog extends AppCompatDialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("Information")
-                .setMessage("Please fill any of two fields")
-
+                .setMessage(getActivity().getString(R.string.enter_code_customer_code))
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
